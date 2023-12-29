f2e429b8a828bc3959212248a7abbe39224ac65b
mjuncobranco <mjuncobranco@gmail.com>
2023-12-29 20:35:12 +0100

    adding img folder

--- js/core.js
+++ js/core.js
@@ -1,4 +1,10 @@
 const GenerateContent = class{
+  fruits = {
+    1: ["01","grapes","uvas",4.95],
+    2: ["02","kiwis","kiwi",3.85],
+   
+  }
+
   getYear = () => {
     let year = new Date()
     document.querySelector(".date").innerHTML+= year.getFullYear();
