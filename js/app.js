a9c62882839524b5c0da10f86df7f572fc94f0f1
mjuncobranco <mjuncobranco@gmail.com>
2024-01-02 16:56:11 +0100

    adding a window event listener type load and selecting the modal alerts by id# set on  keyboard event false

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log('working');
+window.addEventListener("load", () => {
+  let addedToCart = new bootstrap.Modal(
+    document.querySelector("#addedToCart"),
+    {
+      keyboard: false,
+    }
+  );
+  let cartPurchased = new bootstrap.Modal(
+    document.querySelector("#cartPurchased"),
+    {
+      keyboard: false,
+    }
+  );
+  let alreadyAddedToCart = new bootstrap.Modal(
+    document.querySelector("#alreadyAddedToCart"),
+    {
+      keyboard: false,
+    }
+  );
+
+  
+
+});
