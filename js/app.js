28215f5469d2651c8a02f3c95548a8c040c99472
mjuncobranco <mjuncobranco@gmail.com>
2024-01-02 20:56:05 +0100

    generating the row with the new product added to cart, setting min&max quantity, calling toggleShoppingControls to show/hide modal alerts, calling the updateCartTotal to be created next

--- js/app.js
+++ js/app.js
@@ -22,77 +22,162 @@ window.addEventListener("load", () => {
   for (let index = 0; index < addToCartButtons.length; index++) {
     let button = addToCartButtons[index];
     button.addEventListener("click", (e) => {
-      //addToCartClicked(e)
+      addToCartClicked(e);
     });
   }
   //emptying cart
-  document.querySelector("#empty-cart").addEventListener("click",() => {
-    emptyCart();
-  });
+  document
+    .querySelector("#empty-cart")
+    .addEventListener("click", () => emptyCart());
 
   //simulating payment confirmation
-  document.querySelector("#btn-purchase").addEventListener("click",() => {
-    //purchaseClicked();
-  });
+  document
+    .querySelector("#btn-purchase")
+    .addEventListener("click", () => purchaseClicked());
   //stop pulsing effect on shopping cart
-  document.querySelector("#cart").addEventListener("click",()=> {
+  document.querySelector("#cart").addEventListener("click", () => {
     cart.classList.remove("hvr-pulse");
   });
-  //activate/deactivate cart buttons 
+  //activate/deactivate cart buttons
   const toggleShoppingControls = (enable) => {
     if (enable) {
       document.querySelector("#empty-cart").classList.remove("disabled");
       document.querySelector("#btn-purchase").classList.remove("disabled");
-      document.querySelector("#empty-cart").classList.add("hvr-pulse");
-
-
-    }else {
+      document.querySelector("#btn-purchase").classList.add("hvr-pulse");
+    } else {
       document.querySelector("#empty-cart").classList.add("disabled");
-      document.querySelector("#empty-cart").classList.add("disabled");
-      document.querySelector("#empty-cart").classList.remove("hvr-pulse");
-
+      document.querySelector("#btn-purchase").classList.add("disabled");
+      document.querySelector("#btn-purchase").classList.remove("hvr-pulse");
     }
-  }
-  // emptyCart function 
+  };
+  // emptyCart function
   const emptyCart = () => {
-    document.querySelector("tbody").innerHTML= "";
-    //updateCartTotal()
-    toggleShoppingControls(false);
-  }
- 
-   //simulating purchase and show alerts
-   const purchaseClicked = () => {
     document.querySelector("tbody").innerHTML = "";
-    //updateCartTotal()
+    updateCartTotal();
+    toggleShoppingControls(false);
+  };
+
+  //simulating purchase and show alerts
+  const purchaseClicked = () => {
+    document.querySelector("tbody").innerHTML = "";
+    updateCartTotal()
     toggleShoppingControls(false);
     cartPurchased.show();
     setTimeout(() => {
       cartPurchased.hide();
     }, 2000);
-
-  }
+  };
   //remove items from cart
   const removeCartItem = (e) => {
     let buttonClicked = e.target;
     let newRow = buttonClicked.parentNode.parentNode.getAttribute("row-number");
-    document.querySelector(`#$(newRow)`).remove();
+    document.querySelector(`#${newRow}`).remove();
     let numItemsAdded = document.querySelector("tbody").childElementCount;
-    if (childElementCount == 0) {
+    if (numItemsAdded == 0) {
       toggleShoppingControls(false);
-    } else {
-      //updateCartTotal()
-    } 
-  }
+    }
+    updateCartTotal();
+  };
   //add a new item to cart by clicking a cart button
-  const addToCartClicked = () => {
+  const addToCartClicked = (e) => {
     let buttonClicked = e.target;
     let shopItem = buttonClicked.parentNode.parentNode.parentNode;
-    let productId= shopItem.getAttribute("product-id");
-    let title= shopItem.firstElementChild.getAttribute("alt");
-    let productName= shopItem.lastElementChild.firstElementChild.textContent;
-    let price= shopItem.children[1].firstElementChild.innerText.replace("€/Kg","");
-    let imageSrc= shopItem.firstElementChild.src;
+    let productId = shopItem.getAttribute("product-id");
+    let title = shopItem.firstElementChild.getAttribute("alt");
+    let productName = shopItem.lastElementChild.firstElementChild.textContent;
+    let price = shopItem.children[1].firstElementChild.innerText.replace(
+      " € / Kg",
+      ""
+    );
+    let imageSrc = shopItem.firstElementChild.src;
     addItemToCart(productId, title, productName, price, imageSrc);
-    //updateCartTotal();
-  }
+    updateCartTotal();
+  };
+
+  //generate row for cart
+  const addItemToCart = (productId, title, productName, price, imageSrc) => {
+    let rowId = "row-number-" + productId;
+    let cartItems = document.querySelectorAll("tr");
+    for (let index = 0; index < cartItems.length; index++) {
+      if (cartItems[index].getAttribute("id") == rowId) {
+        alreadyAddedToCart.show();
+        setTimeout(() => {
+          alreadyAddedToCart.hide();
+        }, 2500);
+        return;
+      }
+    }
+    
+    let cartRow = document.createElement("tr");
+    cartRow.setAttribute("id", rowId);
+    let itemsAdded = cartItems.length;
+    let cartRowContents = `
+      <th scope="row">${itemsAdded}</th>
+        <td>
+           <div class="card border-success mb-3 text-center">
+              <div class="card-body text-success">
+                 <img src="${imageSrc}" alt="${title}" class="img-thumbnail">
+              </div>
+              <div class="card-footer bg-transparent border-success">${productName}</div>
+           </div>
+        </td>
+        <td>
+           <h4 class="card-title pricing-card-title text-center">${price} € <small class="text-muted">/ Kg</small></h4>
+        </td>
+        <td>
+           <div class="row">
+              <div class="d-flex justify-content-center quantity col-12 col-md-6">
+                 <input class="form-control rounded-sm text-dark border-info" type="number" min="1" max="10"
+                    step="1" value="1" row-number="${rowId}">
+                 <div class="quantity-nav">
+                    <div class="quantity-button quantity-up border-info">
+                      <i class="fa-solid fa-circle-arrow-up text-warning"></i>
+                    </div>
+                    <div class="quantity-button quantity-down border-info">
+                      <i class="fa-solid fa-circle-arrow-down text-warning"></i>
+                    </div>
+                 </div>
+              </div>
+              <div class="text-center col-12 col-md-6">
+                 <button type="button" class="btn btn-link text-danger mt-2" row-number="${rowId}">
+                    <span>
+                    <i class="fa-solid fa-circle-xmark"></i>
+                    </span>
+                 </button>
+              </div>
+           </div>
+        </td>`;
+        cartRow.innerHTML = cartRowContents;
+        document.querySelector("tbody").appendChild(cartRow);
+        cartRow.lastElementChild.lastElementChild.lastElementChild.firstElementChild.addEventListener("click", removeCartItem());
+        cartRow.firstElementChild.firstElementChild.firstElementChild.firstElementChild.addEventListener("change", updateCartTotal());
+        cartRow.firstElementChild.firstElementChild.firstElementChild.firstElementChild.addEventListener("input", updateCartTotal());
+        let input = cartRow.querySelector("input");
+        let max = input.getAttribute("max");
+        let min = input.getAttribute("min");
+        let quantityUp = cartRow.querySelector(".quantity-up");
+        quantityUp.firstElementChild.addEventListener("click", () => {
+          if (parseFloat(input.value) < max) {
+            input.value = parseFloat(input.value) + 1;
+          }
+          updateCartTotal();
+        });
+        let quantityDown = cartRow.querySelector(".quantity-down");
+        quantityDown.addEventListener("click", () => {
+          if (parseFloat(input.value) > min) {
+            input.value = parseFloat(input.value) - 1;
+          }
+          updateCartTotal();
+        });
+
+        document.querySelector("#cart").classList.add("hvr-pulse");
+          if (itemsAdded == 1) {
+          toggleShoppingControls(true);
+        }
+
+        addedToCart.show();
+        setTimeout(() => {
+          addedToCart.hide();
+        }, 1800);
+    };
 });
